3ae93fc4255550ab29aad7eb01075ca3249afbe1
Tommi Lilja <tommi.lilja@icloud.com>
2025-07-02 23:41:30 +0200

    Remove exceptions for null/missing cases

--- src/Farse/Parser.fs
+++ src/Farse/Parser.fs
@@ -25,7 +25,7 @@ module Parser =
 
     let map fn x : Parser<_> = bind (fn >> from) x
 
-    let internal traverseImpl okFn nullFn notFoundFn (path:string) (parser:Parser<_>) : Parser<_> =
+    let internal traverse (path:string) (parser:Parser<_>) : Parser<_> =
         fun (element:JsonElement) ->
             let mutable lastElement = element
             let mutable lastName = String.Empty
@@ -39,40 +39,55 @@ module Parser =
                             lastName <- name
                             lastElement <- state
 
-                            let next = state.GetProperty(name)
-
-                            if next.ValueKind = JsonValueKind.Null then
-                                raise <| NullPropertyException (lastName, lastElement)
-
-                            next
+                            match state.GetProperty(name) with
+                            | prop when prop.ValueKind <> JsonValueKind.Null -> prop
+                            | _ -> raise <| NullPropertyException (lastName, lastElement)
                         | _ -> raise <| NotObjectException (name, state)
                     ) element
 
                 match parser element with
-                | Ok x -> Ok <| okFn x
+                | Ok x -> Ok x
                 | Error e -> Error.parseError lastName e lastElement
             with
                 | NotObjectException (name, element) -> Error.notObject name element
-                | NullPropertyException (name, element) -> nullFn name element
+                | NullPropertyException (name, element) -> Error.nullProperty name element
                 | ArrayException msg -> Error.parseError lastName msg lastElement
-                | :? KeyNotFoundException -> notFoundFn lastName lastElement
+                | :? KeyNotFoundException -> Error.missingProperty lastName lastElement
                 | :? InvalidOperationException -> Error.notObject lastName lastElement
 
-    let internal traverse (path:string) (parser:Parser<_>) : Parser<_> =
-        traverseImpl
-           id
-           Error.nullProperty
-           Error.missingProperty
-           path
-           parser
-
     let internal tryTraverse (path:string) (parser:Parser<_>) : Parser<_> =
-        traverseImpl
-           Some
-           (fun _ _ -> Ok None)
-           (fun _ _ -> Ok None)
-           path
-           parser
+        fun (element:JsonElement) ->
+            let mutable lastElement = element
+            let mutable lastName = String.Empty
+            try
+                let element =
+                    path.Split(".", StringSplitOptions.RemoveEmptyEntries)
+                    |> Array.fold (fun (state:JsonElement option) name ->
+                        state
+                        |> Option.bind (fun state ->
+                            match state.ValueKind with
+                            | JsonValueKind.Null -> None
+                            | JsonValueKind.Object ->
+                                lastName <- name
+                                lastElement <- state
+
+                                match state.TryGetProperty(name) with
+                                | true, prop when prop.ValueKind <> JsonValueKind.Null -> Some prop
+                                | _ -> None
+                            | _ -> raise <| NotObjectException (name, state)
+                        )
+                    ) (Some element)
+
+                match element with
+                | Some element ->
+                    match parser element with
+                    | Ok x -> Ok <| Some x
+                    | Error e -> Error.parseError lastName e lastElement
+                | None -> Ok None
+            with
+                | NotObjectException (name, element) -> Error.notObject name element
+                | ArrayException msg -> Error.parseError lastName msg lastElement
+                | :? InvalidOperationException -> Error.notObject lastName lastElement
 
     /// <summary>Parses a JSON string using the supplied parser.</summary>
     /// <param name="json">The JSON string to parse.</param>
