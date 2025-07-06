be87ab466096c5a05dd8f344afa4cd7f6662fc9b
Tommi Lilja <tommi.lilja@icloud.com>
2025-07-06 15:41:30 +0200

    Refactor

--- src/Farse/Parse.fs
+++ src/Farse/Parse.fs
@@ -6,48 +6,61 @@ open System.Text.Json
 
 module Parse =
 
+    let private parse (name:string) parser =
+        fun (element:JsonElement) ->
+            try
+                match element.ValueKind with
+                | JsonValueKind.Object ->
+                    match element.TryGetProperty(name) with
+                    | true, prop when prop.ValueKind <> JsonValueKind.Null ->
+                        match parser prop with
+                        | Ok x -> Ok x
+                        | Error e -> Error.parseError name e element
+                    | false, _ -> Error.missingProperty name element
+                    | _ -> Error.nullProperty name element
+                | _ -> Error.notObject name element
+            with ArrayException e -> Error.parseError name e element
+
+    let private tryParse (name:string) parser =
+        fun (element:JsonElement) ->
+            try
+                match element.ValueKind with
+                | JsonValueKind.Object ->
+                    match element.TryGetProperty(name) with
+                    | true, prop when prop.ValueKind <> JsonValueKind.Null ->
+                        match parser prop with
+                        | Ok x -> Ok <| Some x
+                        | Error e -> Error.parseError name e element
+                    | _ -> Ok None
+                | _ -> Error.notObject name element
+            with ArrayException e -> Error.parseError name e element
+
     /// <summary>Parses a required property with the supplied parser.</summary>
     /// <param name="path">The path to the property. For example, "prop" or "prop.prop2".</param>
     /// <param name="parser">The parser used to parse the property value. For example, Parse.int.</param>
     let req (path:string) (parser:Parser<_>) : Parser<_> =
         match path with
-        | Flat name ->
-            fun (element:JsonElement) ->
-                try
-                    match element.ValueKind with
-                    | JsonValueKind.Object ->
-                        match element.TryGetProperty(name) with
-                        | true, prop when prop.ValueKind <> JsonValueKind.Null ->
-                            match parser prop with
-                            | Ok x -> Ok x
-                            | Error e -> Error.parseError name e element
-                        | false, _ -> Error.missingProperty name element
-                        | _ -> Error.nullProperty name element
-                    | _ -> Error.notObject name element
-                with ArrayException e -> Error.parseError name e element
+        | Flat name -> parse name parser
         | Nested path ->
-            Parser.traverse path parser
+            parser
+            |> Array.foldBack (fun name parser -> parse name parser)
+                (path.Split('.', StringSplitOptions.RemoveEmptyEntries))
 
     /// <summary>Parses an optional property with the supplied parser.</summary>
     /// <param name="path">The path to the property. For example, "prop" or "prop.prop2".</param>
     /// <param name="parser">The parser used to parse the property value. For example, Parse.int.</param>
     let opt (path:string) (parser:Parser<_>) : Parser<_> =
         match path with
-        | Flat name ->
-            fun (element:JsonElement) ->
-                try
-                    match element.ValueKind with
-                    | JsonValueKind.Object ->
-                        match element.TryGetProperty(name) with
-                        | true, prop when prop.ValueKind <> JsonValueKind.Null ->
-                            match parser prop with
-                            | Ok x -> Ok <| Some x
-                            | Error e -> Error.parseError name e element
-                        | _ -> Ok None
-                    | _ -> Error.notObject name element
-                with ArrayException e -> Error.parseError name e element
+        | Flat name -> tryParse name parser
         | Nested path ->
-            Parser.tryTraverse path parser
+            parser
+            |> Parser.map Some
+            |> Array.foldBack (fun name parser element ->
+                match tryParse name parser element with
+                | Ok (Some v) -> Ok v
+                | Ok None -> Ok None
+                | Error e -> Error e
+            ) (path.Split('.', StringSplitOptions.RemoveEmptyEntries))
 
     let private enumerable convert (parser:Parser<_>) : Parser<_> =
         fun (element:JsonElement) ->
--- src/Farse/Parser.fs
+++ src/Farse/Parser.fs
@@ -1,7 +1,5 @@
 namespace Farse
 
-open System
-open System.Collections.Generic
 open System.Text.Json
 
 type Parser<'a> = JsonElement -> Result<'a, string>
@@ -24,68 +22,6 @@ module Parser =
             | Error e -> Error e
 
     let map fn x : Parser<_> = bind (fn >> from) x
-
-    let internal traverse (path:string) (parser:Parser<_>) : Parser<_> =
-        fun (element:JsonElement) ->
-            let mutable lastElement = element
-            let mutable lastName = String.Empty
-            try
-                let element =
-                    path.Split(".", StringSplitOptions.RemoveEmptyEntries)
-                    |> Array.fold (fun (state:JsonElement) name ->
-                        match state.ValueKind with
-                        | JsonValueKind.Object ->
-                            lastName <- name
-                            lastElement <- state
-
-                            match state.GetProperty(name) with
-                            | prop when prop.ValueKind <> JsonValueKind.Null -> prop
-                            | _ -> raise <| NullPropertyException (name, state)
-                        | _ -> raise <| NotObjectException (name, state)
-                    ) element
-
-                match parser element with
-                | Ok x -> Ok x
-                | Error e -> Error.parseError lastName e lastElement
-            with
-                | NotObjectException (name, element) -> Error.notObject name element
-                | NullPropertyException (name, element) -> Error.nullProperty name element
-                | ArrayException e -> Error.parseError lastName e lastElement
-                | :? KeyNotFoundException -> Error.missingProperty lastName lastElement
-                | :? InvalidOperationException -> Error.notObject lastName lastElement
-
-    let internal tryTraverse (path:string) (parser:Parser<_>) : Parser<_> =
-        fun (element:JsonElement) ->
-            let mutable lastElement = element
-            let mutable lastName = String.Empty
-            try
-                let element =
-                    path.Split(".", StringSplitOptions.RemoveEmptyEntries)
-                    |> Array.fold (fun (state:JsonElement option) name ->
-                        state
-                        |> Option.bind (fun state ->
-                            match state.ValueKind with
-                            | JsonValueKind.Object ->
-                                lastName <- name
-                                lastElement <- state
-
-                                match state.TryGetProperty(name) with
-                                | true, prop when prop.ValueKind <> JsonValueKind.Null -> Some prop
-                                | _ -> None
-                            | _ -> raise <| NotObjectException (name, state)
-                        )
-                    ) (Some element)
-
-                match element with
-                | Some element ->
-                    match parser element with
-                    | Ok x -> Ok <| Some x
-                    | Error e -> Error.parseError lastName e lastElement
-                | None -> Ok None
-            with
-                | NotObjectException (name, element) -> Error.notObject name element
-                | ArrayException e -> Error.parseError lastName e lastElement
-                | :? InvalidOperationException -> Error.notObject lastName lastElement
 
     /// <summary>Parses a JSON string using the supplied parser.</summary>
     /// <param name="json">The JSON string to parse.</param>
