a9b1f34c74830ac600571f228cfa1fe72cff5eb4
Tommi Lilja <tommi.lilja@icloud.com>
2025-08-31 00:56:58 +0200

    Update CE

--- src/Farse/Parser.Builder.fs
+++ src/Farse/Parser.Builder.fs
@@ -12,6 +12,10 @@ module ParserBuilder =
 
         member _.Bind(x, fn) = bind fn x
 
+        member _.Bind(x:Result<_,_>, fn) = fromResult x |> bind fn
+
+        member _.Combine (a, b) = bind (fun () -> b) a
+
         member _.Zero() = from ()
 
         member _.Delay(fn) = fn ()
