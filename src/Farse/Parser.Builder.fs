47d98b7a536114978f74f2bf6f58bd89639d30bc
Tommi Lilja <tommi.lilja@icloud.com>
2025-09-21 00:47:58 +0200

    Update builder

--- src/Farse/Parser.Builder.fs
+++ src/Farse/Parser.Builder.fs
@@ -50,6 +50,80 @@ module ParserBuilder =
                 | _, _, _, Error e, _ -> Error e
                 | _, _, _, _, Error e -> Error e
 
+        member _.Bind2(a: Parser<'a>, b: Parser<'b>, fn: 'a * 'b -> Parser<'c>) =
+            fun element ->
+                match a element, b element with
+                | Ok a, Ok b -> fn (a, b) <| element
+                | Error e, _ -> Error e
+                | _, Error e -> Error e
+
+        member _.Bind3(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, fn: 'a * 'b * 'c -> Parser<'d>) =
+            fun element ->
+                match a element, b element, c element with
+                | Ok a, Ok b, Ok c -> fn (a, b, c) <| element
+                | Error e, _, _ -> Error e
+                | _, Error e, _ -> Error e
+                | _, _, Error e -> Error e
+
+        member _.Bind4(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, d: Parser<'d>, fn: 'a * 'b * 'c * 'd -> Parser<'e>) =
+            fun element ->
+                match a element, b element, c element, d element with
+                | Ok a, Ok b, Ok c, Ok d -> fn (a, b, c, d) <| element
+                | Error e, _, _, _ -> Error e
+                | _, Error e, _, _ -> Error e
+                | _, _, Error e, _ -> Error e
+                | _, _, _, Error e -> Error e
+
+        member _.Bind5(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, d: Parser<'d>, e: Parser<'e>, fn: 'a * 'b * 'c * 'd * 'e -> Parser<'f>) =
+            fun element ->
+                match a element, b element, c element, d element, e element with
+                | Ok a, Ok b, Ok c, Ok d, Ok e -> fn (a, b, c, d, e) <| element
+                | Error e, _, _, _, _ -> Error e
+                | _, Error e, _, _, _ -> Error e
+                | _, _, Error e, _, _ -> Error e
+                | _, _, _, Error e, _ -> Error e
+                | _, _, _, _, Error e -> Error e
+
+        member _.BindReturn(a: Parser<'a>, fn: 'a -> 'b) =
+            fun element ->
+                match a element with
+                | Ok a -> Ok <| fn a
+                | Error e -> Error e
+
+        member _.Bind2Return(a: Parser<'a>, b: Parser<'b>, fn: 'a * 'b -> 'c) =
+            fun element ->
+                match a element, b element with
+                | Ok a, Ok b -> Ok <| fn (a, b)
+                | Error e, _ -> Error e
+                | _, Error e -> Error e
+
+        member _.Bind3Return(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, fn: 'a * 'b * 'c -> 'd) =
+            fun element ->
+                match a element, b element, c element with
+                | Ok a, Ok b, Ok c -> Ok <| fn (a, b, c)
+                | Error e, _, _ -> Error e
+                | _, Error e, _ -> Error e
+                | _, _, Error e -> Error e
+
+        member _.Bind4Return(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, d: Parser<'d>, fn: 'a * 'b * 'c * 'd -> 'e) =
+            fun element ->
+                match a element, b element, c element, d element with
+                | Ok a, Ok b, Ok c, Ok d -> Ok <| fn (a, b, c, d)
+                | Error e, _, _, _ -> Error e
+                | _, Error e, _, _ -> Error e
+                | _, _, Error e, _ -> Error e
+                | _, _, _, Error e -> Error e
+
+        member _.Bind5Return(a: Parser<'a>, b: Parser<'b>, c: Parser<'c>, d: Parser<'d>, e: Parser<'e>, fn: 'a * 'b * 'c * 'd * 'e -> 'f) =
+            fun element ->
+                match a element, b element, c element, d element, e element with
+                | Ok a, Ok b, Ok c, Ok d, Ok e -> Ok <| fn (a, b, c, d, e)
+                | Error e, _, _, _, _ -> Error e
+                | _, Error e, _, _, _ -> Error e
+                | _, _, Error e, _, _ -> Error e
+                | _, _, _, Error e, _ -> Error e
+                | _, _, _, _, Error e -> Error e
+
         member _.Zero() = from ()
 
         member _.Delay(fn) = fn ()
