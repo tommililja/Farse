4f6aa355bc941d99a6243f89f3ae1082f45b61ed
Tommi Lilja <tommi.lilja@icloud.com>
2025-12-03 21:48:44 +0100

    Update example

--- src/Farse.Tests/Example.fs
+++ src/Farse.Tests/Example.fs
@@ -84,7 +84,7 @@ module Parse =
         Parse.custom (fun element ->
             match element.TryGetGuid() with
             | true, guid -> Ok <| ProfileId guid
-            | _ -> Error <| Some "Invalid guid." // Added as details.
+            | _ -> Error None // No details.
         ) ExpectedKind.String
 
     let instant =
