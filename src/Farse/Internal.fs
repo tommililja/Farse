77b84347c9e0f8956d843ce584d633f34a869d77
Tommi Lilja <tommi.lilja@icloud.com>
2026-09-14 21:43:30 +0200

    Fix build error

--- src/Farse/Internal.fs
+++ src/Farse/Internal.fs
@@ -144,7 +144,7 @@ module internal Internal =
             | ExpectedKind.Object -> e.ValueKind = Kind.Object
             | ExpectedKind.String -> e.ValueKind = Kind.String
 
-        let inline (|Prop|Path|) (path: string) =
+        let (|Prop|Path|) (path:string) =
             let segments =
                 pathRegex.Matches(path)
                 |> Seq.map _.Value.Replace("\\.", ".")
